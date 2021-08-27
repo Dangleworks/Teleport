b27d93616ea3da05666961453bba9191836a20da
Alan Litz <alanjlitz@gmail.com>
2021-08-26 22:42:53 -0400

    this is my plugin now

--- script.lua
+++ script.lua
@@ -1,5 +1,7 @@
 zones = {}
 aliases = {}
+tpq = {}
+
 function onCreate(is_world_create)
 	zones = server.getZones("type=teleport")
 	for i,e in ipairs(zones) do
@@ -10,14 +12,31 @@ function onCreate(is_world_create)
 end
 
 function onCustomCommand(full_message, user_peer_id, is_admin, is_auth, command, ...)
-	args = {...}
+	local args = {...}
 	command = string.lower(command)
 	if (command == "?tp") then
-		dest = aliases[args[1]]
-		if dest ~= nil then
-			server.setPlayerPos(user_peer_id, zones[dest].transform)
-			server.setPlayerPos(user_peer_id, zones[dest].transform)
+		if args[1] == nil then
+			server.announce("[TP]", "Destination required", user_peer_id)
+			return
+		end
+		local dest = aliases[args[1]]
+		if dest == nil then
+			server.announce("[TP]", "Invalid destination", user_peer_id)
+			return
+		end
+		
+		server.setPlayerPos(user_peer_id, zones[dest].transform)
+		tpq[user_peer_id] = {time=server.getTimeMillisec(), dest=zones[dest].transform}
+	end
+end
+
+
+function onTick(ticks)
+	ctime = server.getTimeMillisec()
+	for pid, obj in pairs(tpq) do
+		if ctime - obj.time >= 100 then
+			server.setPlayerPos(pid, obj.dest)
+			tpq[pid] = nil
 		end
 	end
-
 end
